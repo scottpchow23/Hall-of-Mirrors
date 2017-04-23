0c8af288694360b26db3443fd84ecc35b7be257b
Scott Chow <scottpchow@umail.ucsb.edu>
2017-04-23 13:18:57 -0700

    Changed up the base class to play around with

--- ReflectionTesting.playground/Contents.swift
+++ ReflectionTesting.playground/Contents.swift
@@ -1,5 +1,20 @@
-//: Playground - noun: a place where people can play
+//: Reflection - noun: an image seen in a mirror or shiny surface
 
 import UIKit
 
-var str = "Hello, playground"
+class Project {
+    var title: String = ""
+    var id: Int = 0
+    var platform: String = ""
+    var version: Double = 0.0
+    var info: String?
+}
+
+let project = Project()
+project.title = "Reflection"
+project.id = 4302667
+project.platform = "Xcode 8.3"
+project.version = 0.1
+project.info = "This is an example of reflection being used in Swift."
+
+print(project.title)
